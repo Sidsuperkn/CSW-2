21905d84b5d54185f3667847fe9da24413c94564
Sidsuperkn <122125106+Sidsuperkn@users.noreply.github.com>
2024-05-16 16:39:49 +0530

    Add files via upload

---
+++ Assignment_8/2241011191/src/_2241011191_Ass8_Q3.java
@@ -0,0 +1,33 @@
+import java.io.FileWriter;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Scanner;
+
+public class _2241011191_Ass8_Q3 {
+    public static void main(String[] args) {
+        String fileName = "diary.txt";
+
+        try {
+            FileWriter writer = new FileWriter(fileName, true);
+
+            SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+            String currentDate = dateFormat.format(new Date());
+
+            Scanner sc = new Scanner(System.in);
+            System.out.println("Enter your new diary entry:");
+            String newEntry = sc.nextLine();
+
+            writer.write("\nDate: " + currentDate + "\n");
+            writer.write("Entry: " + newEntry + "\n");
+
+            writer.close();
+            sc.close();
+
+            System.out.println("New diary entry has been successfully added to '" + fileName + "'.");
+
+        } catch (IOException e) {
+            System.out.println("An error occurred while appending to the file: " + e.getMessage());
+        }
+    }
+}
